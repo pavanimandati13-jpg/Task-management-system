05b08e4d32c8844c47e1c7648e758307e316a01c
pavanimandati13-jpg <pavanimandati13@gmail.com>
2026-07-31 23:48:02 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def home():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+    tasks.append(task)
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    tasks.pop(id)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
